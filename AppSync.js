fd264e26ede4084567fb5d9c03dd43c0470562a7
Nick-OD <odonnell.nick28@gmail.com>
2018-04-27 14:10:00 -0400

    My Changes, Google Login Feature

---
+++ AppSync.js
@@ -0,0 +1,6 @@
+﻿export default {
+    "graphqlEndpoint": "https://db3jyx6kubdh7ansjiulmdrv7y.appsync-api.us-east-1.amazonaws.com/graphql",
+    "region": "us-east-1",
+    "authenticationType": "API_KEY",
+    "apiKey": "da2-25jeia2zy5hzrgk6vw72wydye4"
+}
